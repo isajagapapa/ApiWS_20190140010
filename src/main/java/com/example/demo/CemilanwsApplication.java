d852f1c082bf05396796be8b53b0a95b5c2f2c33
isss7 <faisfidi@gmail.com>
2021-12-31 14:29:56 +0700

    first

---
+++ src/main/java/com/example/demo/CemilanwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CemilanwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CemilanwsApplication.class, args);
+	}
+
+}
